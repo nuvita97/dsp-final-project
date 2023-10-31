58a848f11e17109616d581f3dc1fac534a1b6e30
Christian Dirisu <christian.dirisu@gmail.com>
2023-10-31 16:18:12 +0100

    connected database

--- api-db/app.py
+++ api-db/app.py
@@ -1,43 +1,43 @@
-# 1. Library imports
-import uvicorn
 from fastapi import FastAPI
-from rating import Rating
-import numpy as np
+from pydantic import BaseModel
 import pickle
-import pandas as pd
-# 2. Create the app object
+from bs4 import BeautifulSoup
+from unidecode import unidecode
+import re
+from main import psycopg2
+# from sklearn.feature_extraction.text import TfidfVectorizer
+
 app = FastAPI()
-pickle_in = open("dsp_project_model.pkl","rb")
-classifier=pickle.load(pickle_in)
 
-# 3. Index route, opens automatically on http://127.0.0.1:8000
-@app.get('/')
-def index():
-    return {'message': 'Hello, Product Owner'}
+# Load the trained model and TF-IDF vectorizer from the pickle file
+with open("dsp_project_model.pkl", "rb") as model_file:
+    model = pickle.load(model_file)
 
-# 4. Route with a single parameter, returns the parameter within a message
-#    Located at: http://127.0.0.1:8000/AnyNameHere
-@app.get('/{name}')
-def get_name(name: str):
-    return {'Welcome to Product Rating': f'{name}'}
+with open("dsp_tfidf.pkl", "rb") as model_file:
+    tfidf = pickle.load(model_file)   
 
-# 3. Expose the prediction functionality, make a prediction from the passed
-#    JSON data and return the predicted results from the review text
-@app.post('/predict')
-def predict_rating(data: Rating):
-    data = data.dict()
-    reviewText = data['reviewText']
-    # overall=['overall']
-    
-   # print(classifier.predict([[overall]]))
-    prediction = classifier.predict([reviewText])
-    return {
-        'prediction': prediction[0]
-    }
+# Define the text preprocessing function
+def clean_text(text):
+    text = str(text).lower().replace('\\', '').replace('_', ' ')
+    text = re.sub(r'\S+@\S+', '', text)
+    text = re.sub(r'http\S+', '', text)
+    soup = BeautifulSoup(text, 'html.parser')
+    text = soup.get_text()
+    text = unidecode(text)
+    text = re.sub(r'[^a-zA-Z0-9\s]', '', text)
+    text = re.sub(r"(.)\\1{2,}", "\\1", text)
+    return text
 
-# 5. Run the API with uvicorn
-#    Will run on http://127.0.0.1:8000
-if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8000)
-    
-#uvicorn app:app --reload
+def save_predictions(review, rating):
+    # 3. Make a connection to the database
+    conn = psycopg2.connect("dbname=amazon-reviews user=postgres password=password")
+    # Create a cursor
+    cur = conn.cursor()
+    # Define the query
+    sql = """INSERT INTO predictions(review, rating, timestamp) 
+        VALUES('this new review', 5, NOW() ) RETURNING id;"""
+    # Perform the query
+    cur.execute(sql)
+    # Commit and close
+    conn.commit()     
+    cur.close()
--- api-db/main.py
+++ api-db/main.py
@@ -1,6 +1,12 @@
 # 1. Library imports
 import uvicorn
 from fastapi import FastAPI
+import psycopg2
+from rating import Rating
+from app import clean_text
+from app import tfidf
+from app import model
+from app import save_predictions
 
 # 2. Create the app object
 app = FastAPI()
@@ -15,6 +21,24 @@ def index():
 @app.get('/Welcome')
 def get_name(name: str):
     return {'Welcome to Product Rating': f'{name}'}
+
+@app.post("/predict")
+def predict(data: Rating):
+    review_text = data.reviewText
+
+    # Clean and preprocess the input text
+    cleaned_text = clean_text(review_text)
+
+    # Transform the preprocessed text using the TF-IDF vectorizer
+    text_vector = tfidf.transform([cleaned_text])
+
+    # Predict the rating using the trained model
+    rating = model.predict(text_vector)  # Use the predict method, not subscripting
+
+    prediction_id = save_predictions(review_text, int(rating))
+
+    return {"rating": rating[0]}  # Access the first element of the prediction
+
 
 # 5. Run the API with uvicorn
 #    Will run on http://127.0.0.1:8000
