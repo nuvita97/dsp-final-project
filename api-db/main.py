3a3991d3424adbbd3a42e739aa19cfc2b3abfcb2
Viet-Thai NGUYEN <vietthai2303@gmail.com>
2023-12-04 16:29:42 +0100

    Show History button

--- api-db/main.py
+++ api-db/main.py
@@ -3,6 +3,7 @@ import psycopg2
 import pickle
 from functions import Rating
 from functions import clean_text, save_prediction
+from typing import List
 
 
 app = FastAPI()
@@ -16,13 +17,24 @@ with open("../model/dsp_project_tfidf_model.pkl", "rb") as model_file:
 
 # Predict and save predictions
 @app.post("/predict/")
-def predict(data: Rating):
-    review_text = data.review
-    cleaned_text = clean_text(review_text)
-    text_vector = tfidf.transform([cleaned_text])
-    rating = model.predict(text_vector)
-    save_prediction(str(review_text), int(rating))
-    return {"rating": rating[0]} 
+def predict(data: List[Rating]):
+    
+    predictions = []
+    review_texts = df["reviewText"].tolist()
+    for review_text in review_texts:
+        cleaned_text = clean_text(review_text)
+        text_vector = tfidf.transform([cleaned_text])
+        rating = model.predict(text_vector)
+        save_prediction(str(review_text), int(rating))
+        predictions.append({"review": review_text, "rating": rating[0]})
+    return predictions
+
+    # review_text = data.review
+    # cleaned_text = clean_text(review_text)
+    # text_vector = tfidf.transform([cleaned_text])
+    # rating = model.predict(text_vector)
+    # save_prediction(str(review_text), int(rating))
+    # return {"rating": rating[0]} 
 
 
 # Get all predictions
