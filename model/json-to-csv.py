b9ce8952bddc53d10708ef2bc4c88cab17c085f5
Stephanie Arthaud <slr.arthaud@gmail.com>
2023-12-05 14:07:38 +0100

    added print to verify json rows and csv rows

--- model/json-to-csv.py
+++ model/json-to-csv.py
@@ -1,30 +1,33 @@
 import json
 import csv
 
-# read JSON data and create a list of dictionaries
 data = []
 with open('Kindle_Store_5_2018.json', 'r') as json_file:
     for line in json_file:
         entry = json.loads(line)
         if 'vote' not in entry:
-            entry['vote'] = "0"  # set 'vote' to "0" if not present
+            entry['vote'] = "0"  
         data.append(entry)
 
-# modify the 'style' column
+num_data_rows_json = len(data)
+
 for entry in data:
     if 'style' in entry:
         style_value = entry['style'].get('Format:', '')
         entry['style'] = style_value
 
-# determine the fieldnames dynamically based on the keys in the data
 fieldnames = set()
 for entry in data:
     fieldnames.update(entry.keys())
 
-# write the json data to a CSV file
 with open('Kindle_Store_5_2018.csv', 'w', newline='') as csv_file:
     writer = csv.DictWriter(csv_file, fieldnames=list(fieldnames))
     
     writer.writeheader()
     for entry in data:
         writer.writerow(entry)
+
+num_data_rows_csv = len(data)
+
+print(f"Number of data rows in JSON: {num_data_rows_json}")
+print(f"Number of data rows in CSV output: {num_data_rows_csv}")
