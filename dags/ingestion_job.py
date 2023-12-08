cad30f25a825d14dee5dbb14be33f32e4396c7bb
Stephanie Arthaud <slr.arthaud@gmail.com>
2023-12-08 08:05:17 +0100

    one csv file per task, remove when processed

--- dags/ingestion_job.py
+++ dags/ingestion_job.py
@@ -6,16 +6,6 @@ from airflow import DAG
 from airflow.decorators import task
 from great_expectations.dataset import PandasDataset
 
-# TODO
-# check other conditions: 
-# - null / not null "reviewText"                                            (X)
-# - if column "reviewText" exists                                           (_)
-# - check reviewText has string datatype                                    (_)
-# - more than 500 characters                                                (_)
-# One job is one csv file (1min(variable) for each csv file)                (_)
-# csv files in Folder B, create column and define the 4 errors (simplified) (_)
-
-
 FOLDER_A = 'Folder_A_test'
 FOLDER_B = 'Folder_B_ingested_test'
 FOLDER_C = 'Folder_C_ingested_test'
@@ -31,45 +21,49 @@ def ingest_data(file_path):
     logger.info(f'Starting to read {file_path}')
     df = pd.read_csv(file_path)
 
-    # Great Expectations checks
     ge_df = PandasDataset(df)
     if not ge_df.expect_column_to_exist('reviewText')['success']:
-        logger.error('reviewText column does not exist in the DataFrame')
+        logger.info('reviewText column does not exist in the DataFrame')
         return
 
-    # Check the reviewText column
     non_null_df = df[df['reviewText'].notna()]
     null_df = df[df['reviewText'].isna()]
 
     if len(non_null_df) == len(df):
-        # All values are non-null
+        # all values in "reviewText" are non-null -> Folder_B
         output_path = os.path.join(FOLDER_C, 'ingested_' + datetime.now().strftime('%Y%m%d%H%M%S') + '.csv')
         non_null_df.to_csv(output_path, index=False)
         logger.info(f'Success: All non-null rows ingested and saved to {output_path}')
     elif len(null_df) == len(df):
-        # All values are null
+        # all values in "reviewText" are null -> Folder_C
         output_path = os.path.join(FOLDER_B, 'ingested_' + datetime.now().strftime('%Y%m%d%H%M%S') + '.csv')
         null_df.to_csv(output_path, index=False)
         logger.info(f'Success: All null rows ingested and saved to {output_path}')
     else:
-        # Mixed values
+        # if values are mixed non-null and null
         output_path_non_null = os.path.join(FOLDER_C, 'ingested_' + datetime.now().strftime('%Y%m%d%H%M%S') + '_non_null.csv')
         output_path_null = os.path.join(FOLDER_B, 'ingested_' + datetime.now().strftime('%Y%m%d%H%M%S') + '_null.csv')
         non_null_df.to_csv(output_path_non_null, index=False)
         null_df.to_csv(output_path_null, index=False)
         logger.info(f'Success: Split mixed rows and saved to {output_path_non_null} and {output_path_null}')
 
+    try:
+        os.remove(file_path)
+        logger.info(f'Successfully removed {file_path}')
+    except Exception as e:
+        logger.error(f'Error while removing {file_path}: {str(e)}')
+
 with DAG(
     dag_id='ingestion_job',
     default_args=default_args,
     description='A DAG for ingesting CSV files',
-    schedule_interval=timedelta(minutes=2),
-    start_date=datetime.now() - timedelta(hours=1),
-    tags=['example'],
+    schedule_interval=timedelta(minutes=1),
+    start_date=datetime.now() - timedelta(hours=1)
 ) as dag:
 
     for file in os.listdir(FOLDER_A):
         if file.endswith('.csv'):
-            ingest_data_task = ingest_data(os.path.join(FOLDER_A, file))
+            file_path = os.path.join(FOLDER_A, file)
+            ingest_data_task = ingest_data(file_path)
 
 ingest_data_dag = dag
